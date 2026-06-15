11618ed7baa93de8b90b26f8370b09e6762fb2cf
bhargavi132005 <ptbhargavi13@gmail.com>
2026-06-15 14:12:47 +0530

    feat: wire up event and seat status API routes

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getEvents } from '../controllers/eventController.js';
+import { getEvents, getEventById, getEventSeats } from '../controllers/eventController.js';
 import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', protect, getEvents);
+router.get('/:id', protect, getEventById);
+router.get('/:id/seats', protect, getEventSeats);
 
 export default router;
